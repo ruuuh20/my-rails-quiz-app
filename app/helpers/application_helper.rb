d356ed00af80e2079a0da5c920b3c7d00745e164
ruuuh20 <pkim2320@gmail.com>
2017-11-14 13:44:44 -0500

    header and home

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  # Returns true if the user is logged in, false otherwise.
+ def logged_in?
+   !current_user.nil?
+ end
+ 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+  get 'static_pages/home'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'static_pages#home'
+  resources :courses do
+    resources :quizzes
+  end
+
 end
